1ada5a642e0fc184d35cd71c192e5fc2a3a69569
igitego <igitegoberwachanisse@gmail.com>
2025-06-04 16:16:42 +0200

    This is my first commit in my life

---
+++ select_marks.php
@@ -0,0 +1,25 @@
+Mark_Id 
+• Trainee_Id 
+• Module_Id 
+• Formative_Assessment 
+• Summative_Assessment 
+• Total_Marks 
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table border="2" cellpadding="20" cellspacing="2">
+        <tr>
+            <th></th>
+        </tr>
+
+    </table>
+</body>
+</html>
