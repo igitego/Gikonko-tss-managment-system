170c23c6c0b6e9ed209a18b11982253dc736b78e
igitego <igitegoberwachanisse@gmail.com>
2025-06-04 15:14:16 +0200

    This is my first commit in my life

--- insert_trade.php
+++ insert_trade.php
@@ -8,11 +8,34 @@
 <body>
     <form action=""method="POST">
 
-<label for=""></label>
-
-
-
-
-    </form>
+<label for="">trade_name</label>
+<input type="text"name="Trade_name"><br><br>
+<button name="insert">Add</button>
+</form>
 </body>
 </html>
+<?php
+include("conn.php");
+
+if(isset($_POST['insert'])){
+
+    $trade_name=$_POST['Trade_name'];
+
+     $sql = "INSERT INTO trades (Trade_name) VALUES('$trade_name')";
+
+         $query = mysqli_query($conn, $sql);
+
+         if($query)
+
+         {
+
+            echo"trade is inserted";
+         }
+
+         else
+
+         {
+            echo"trade not inserted";
+         }
+}
+?>
